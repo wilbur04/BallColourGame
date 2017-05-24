441eb5484d348108ff0238186f1f4d973857e039
jericaT <zavc166@live.rhul.ac.uk>
2017-05-24 17:20:04 +0100

    Merged

--- app/src/main/java/com/llamaniac/games/ballcolourgame/StartActivity.java
+++ app/src/main/java/com/llamaniac/games/ballcolourgame/StartActivity.java
@@ -3,27 +3,39 @@ package com.llamaniac.games.ballcolourgame;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.RelativeLayout;
 
 public class StartActivity extends AppCompatActivity  implements View.OnClickListener{
-    private Button startBtn, highScoreBtn, helpBtn;
+    private Button startBtn, highScoreBtn, helpBtn, doneBtn;
     private TextView title;
+    private RelativeLayout helpLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
         Typeface customFont = Typeface.createFromAsset(getAssets(), "fonts/font.ttf");
+
         startBtn = (Button) findViewById(R.id.startButton);
         highScoreBtn = (Button) findViewById(R.id.highscoreButton);
         helpBtn = (Button) findViewById(R.id.helpButton);
         title = (TextView) findViewById(R.id.Title);
+        doneBtn = (Button) findViewById(R.id.doneButton);
+        helpLayout = (RelativeLayout) findViewById(R.id.helpScreen);
+
         startBtn.setOnClickListener(this);
+        doneBtn.setOnClickListener(this);
+        helpLayout.setOnClickListener(this);
+
+
+        helpLayout.setVisibility(View.INVISIBLE);
+        helpLayout.bringToFront();
+
         startBtn.setTypeface(customFont);
         highScoreBtn.setTypeface(customFont);
         helpBtn.setTypeface(customFont);
@@ -36,6 +48,12 @@ public class StartActivity extends AppCompatActivity  implements View.OnClickLis
         switch (v.getId()) {
             case R.id.startButton:
                 launchActivity(MainActivity.class);
+                break;
+            case R.id.helpButton:
+                helpLayout.setVisibility(View.VISIBLE);
+                break;
+            case R.id.doneButton:
+                helpLayout.setVisibility(View.INVISIBLE);
                 break;
 
         }
